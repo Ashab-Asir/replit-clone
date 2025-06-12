5e007b2e50373954e674b56e92115a3c01e9e8bd
Ashab-Asir <ashabasir101@gmail.com>
2025-06-12 16:17:05 +0600

    added react query for fetching data

---
+++ replit-backend/src/controllers/pingController.js
@@ -0,0 +1,3 @@
+export default async function ping(req, res) {
+  return res.json({ message: "pong" });
+}
--- replit-backend/src/index.js
+++ replit-backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { PORT } from "./config/serverConfig.js";
+import apiRouter from "./routes/index.js";
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.use("/api", apiRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
