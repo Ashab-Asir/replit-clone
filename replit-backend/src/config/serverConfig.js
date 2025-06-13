03c7eaf87f6fa0d74344bf06f1aabb6664495852
Ashab-Asir <ashabasir101@gmail.com>
2025-06-13 11:23:43 +0600

    added editor component

--- replit-backend/src/config/serverConfig.js
+++ replit-backend/src/config/serverConfig.js
@@ -1,3 +1,4 @@
 import donenv from "dotenv";
 donenv.config();
 export const PORT = process.env.PORT || 3000;
+export const REACT_PROJECT_COMMAND = process.env.REACT_PROJECT_COMMAND;
--- replit-backend/src/controllers/projectController.js
+++ replit-backend/src/controllers/projectController.js
@@ -2,15 +2,13 @@ import util from "util";
 import child_porcess from "child_process";
 import { v4 as uuid } from "uuid";
 import fs from "fs/promises";
+import { REACT_PROJECT_COMMAND } from "../config/serverConfig.js";
 const execPromisify = util.promisify(child_porcess.exec);
 export const createProjectController = async (req, res) => {
   const projectid = uuid();
   await fs.mkdir(`./projects/${projectid}`);
-  const response = await execPromisify(
-    "npm create vite@latest react-project -- --template react",
-    {
-      cwd: `./projects/${projectid}`,
-    }
-  );
+  const response = await execPromisify(REACT_PROJECT_COMMAND, {
+    cwd: `./projects/${projectid}`,
+  });
   return res.json({ message: "Project Created", data: projectid });
 };
