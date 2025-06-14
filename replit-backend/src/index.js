d50ba138ec9e1f96ca513beb59465365bad3f9fb
Ashab-Asir <ashabasir101@gmail.com>
2025-06-14 11:21:52 +0600

    added basic setup for socket io in frontend and backend

--- replit-backend/src/index.js
+++ replit-backend/src/index.js
@@ -1,19 +1,30 @@
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import { PORT } from "./config/serverConfig.js";
 import apiRouter from "./routes/index.js";
 const app = express();
-
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
+io.on("connect", (socket) => {
+  console.log("a user connected");
+});
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.use("/api", apiRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
